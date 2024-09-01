b7be358a50cbe35c16f27ed73b29c1c6c19dd1ae
otonielnn <otoniel_2002@live.com>
2024-09-01 00:50:54 -0300

    Iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange
+
+    response = client.get("/")  # Act
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == "Olá Mundo"
